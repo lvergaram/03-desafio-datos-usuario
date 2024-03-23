7788129d99163f51e2b9de3e2fe31a1e51d2648f
lvergaram <lvergaramena@gmail.com>
2024-03-23 16:57:15 -0300

    init

---
+++ src/main.js
@@ -0,0 +1,19 @@
+// IIFE
+
+const datosUsuariosApp = ( ()=> {
+
+    async privateGetData
+
+    //public
+    return {
+        getData(){console.log('Los datos de usuario son:')}
+    }
+
+
+
+})()
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    datosUsuariosApp.getData()
+})
